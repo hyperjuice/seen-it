4601e76fab363d6c0e034f417568895f594fe1d9
Darrell Cohn <darrellcohn@gmail.com>
2015-05-10 19:25:58 -0700

    Fix pundit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,8 +27,9 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
-
+    
+    # POST associated with logged in user
+    @post = current_user.posts.build(post_params)
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
@@ -43,8 +44,8 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    authorize @post
     respond_to do |format|
+    authorize @post  
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
