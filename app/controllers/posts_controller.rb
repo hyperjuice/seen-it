b5653ddf717c875f445d4f418054d93289a20bdf
RasheedRomain <rj.romain12@gmail.com>
2015-05-12 15:29:41 -0700

    merge conflicts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -92,6 +92,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :text)
+      params.require(:post).permit(:name, :content)
     end
 end
