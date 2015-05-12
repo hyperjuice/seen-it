25547e2d700758b6b441c5f1dbea465aaf101ff4
Darrell Cohn <darrellcohn@gmail.com>
2015-05-11 21:30:33 -0700

    Change alert weight and color

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,7 +62,7 @@ class PostsController < ApplicationController
     authorize @post
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed' }
       format.json { head :no_content }
     end
   end
