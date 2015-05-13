d6ec18b29c87123c64d75a1d1c16ff440a3a5413
Darrell Cohn <darrellcohn@gmail.com>
2015-05-13 14:55:51 -0700

    Pundit for profile pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,15 +1,17 @@
 class PicturesController < ApplicationController
 	def create
 		@picture = Picture.create(picture_params)
+		authorize @picture
 		redirect_to @picture.user	
 	end
 
-def destroy
-	@picture = Picture.find(params[:id])
-	@user = @picture.user
-	@picture.destroy
-	redirect_to @user
-end
+	def destroy
+		@picture = Picture.find(params[:id])
+		authorize @picture
+		@user = @picture.user
+		@picture.destroy
+		redirect_to @user
+	end
 
 private
 	def picture_params
--- app/policies/picture_policy.rb
+++ app/policies/picture_policy.rb
@@ -6,4 +6,12 @@ class PicturePolicy < ApplicationPolicy
     @picture = picture
   end
 
+  def create?
+  	picture.user_id == user.try(:id)
+  end
+
+  def destroy?
+  	picture.user_id == user.try(:id)
+  end
+
 end
