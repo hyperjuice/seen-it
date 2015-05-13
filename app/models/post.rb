529413d517369ce5e3465527982e738f56b8b640
Darrell Cohn <darrellcohn@gmail.com>
2015-05-13 16:04:55 -0700

    Fix add new post tag-bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	attr_accessor :tag
 	belongs_to :user
 
 	acts_as_votable
@@ -6,5 +7,6 @@ class Post < ActiveRecord::Base
 	has_and_belongs_to_many :tags
 
 	validates :name, presence: true
-	has_many :comments 
+	has_many :comments
+
 end
